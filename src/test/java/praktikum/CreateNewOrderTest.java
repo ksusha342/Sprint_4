e17f96b54f00e3963b35934e8a8fa72395dfaf47
Kseniya Zinoveva <ksusha342@yandex.ru>
2023-08-21 17:55:29 +0300

    Add additional 6 tests in OpenAccordionContentTest.java and delete isCreated in CreateNewOrderTest.java

--- src/test/java/praktikum/CreateNewOrderTest.java
+++ src/test/java/praktikum/CreateNewOrderTest.java
@@ -6,21 +6,18 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import praktikum.pages.OrderPage;
 
-import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class CreateNewOrderTest {
-    private final boolean isCreated;
     private final String customerName;
     private final String customerSurname;
     private final String customerAddress;
     private final String customerPhoneNumber;
-    public CreateNewOrderTest(String customerName, String customerSurname, String customerAddress, String customerPhoneNumber, boolean isCreated) {
+    public CreateNewOrderTest(String customerName, String customerSurname, String customerAddress, String customerPhoneNumber) {
         this.customerName = customerName;
         this.customerSurname = customerSurname;
         this.customerAddress = customerAddress;
         this.customerPhoneNumber = customerPhoneNumber;
-        this.isCreated = isCreated;
     }
 
     @Rule
@@ -28,8 +25,8 @@ public class CreateNewOrderTest {
     @Parameterized.Parameters
     public static Object[][] createOrder() {
         return new Object[][] {
-                { "Ян", "Ли", "Ивантеевская улица, 23",  "+7999999999", true},
-                { "Мариня", "Петрова", "Красная площадь", "89111111111", true},
+                { "Ян", "Ли", "Ивантеевская улица, 23",  "+7999999999"},
+                { "Мариня", "Петрова", "Красная площадь", "89111111111"},
         };
     }
 
@@ -49,8 +46,7 @@ public class CreateNewOrderTest {
                 .typeRentalPeriod()
                 .clickBlackColorScooter()
                 .clickCreateOrder()
-                .confirmCreateOrder();
-
-        assertEquals(isCreated, orderPage.hasCreateOrderText());
+                .confirmCreateOrder()
+                .hasCreateOrderText();
     }
 }
